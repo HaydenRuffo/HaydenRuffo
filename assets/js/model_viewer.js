f46644d04d85264ff141d90c6896260727be726d
Nicholas <nchalkley2@student.gsu.edu>
2024-07-25 13:03:24 -0400

    Update model_viewer.js

--- assets/js/model_viewer.js
+++ assets/js/model_viewer.js
@@ -1,1 +1,15 @@
+const modelViewer = document.querySelector("model-viewer");
 
+window.switchSrc = (element, name) => {
+  const base = "https://github.com/HaydenRuffo/Portfolio-Models/raw/main/" + name + ".gltf";
+  modelViewer.src = base;
+  modelViewer.poster = 'https://modelviewer.dev/assets/ShopifyModels/Mixer.webp';
+  const slides = document.querySelectorAll(".slide");
+  slides.forEach((element) => {element.classList.remove("selected");});
+  element.classList.add("selected");
+};
+
+document.querySelector(".slider").addEventListener('beforexrselect', (ev) => {
+  // Keep slider interactions from affecting the XR scene.
+  ev.preventDefault();
+});
